2d99c68c2c6b5775366c833a84227b2fffc38eb7
James Little <littleguy23@gmail.com>
2020-06-21 20:43:32 -0700

    Initial pathfinding working

--- content/static/boids.js
+++ content/static/boids.js
@@ -22,8 +22,15 @@ let aVertexPosition;
 // Program Options
 let boids = [];
 let boidsCount = 200;
-let boidsVelocity = 100;
-const colors = ["#5F0F40", "#9A031E", "#FB8B24", "#E36414", "#0F4C5C"];
+let boidsVelocity = 400;
+const colors = [
+  "#ffa297",
+  "#fba900",
+  "#9dca00",
+  "#00d5a1",
+  "#42cbff",
+  "#c3b1ff",
+];
 
 function randomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -66,8 +73,11 @@ function startup() {
       xPos: randomBetween(-glCanvas.width, glCanvas.width),
       yPos: randomBetween(-glCanvas.height, glCanvas.height),
       theta: randomBetween(0, 2 * Math.PI, false),
+      special: false,
     });
   }
+
+  boids[0].special = true;
 
   const shaderSet = [
     {
@@ -125,7 +135,7 @@ function compileShader(id, type) {
   return shader;
 }
 function animateScene() {
-  gl.clearColor(0.1, 0.0, 0.0, 1.0);
+  gl.clearColor(0.13, 0.21, 0.18, 1.0); // Background color, magic number!
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Load vertices
@@ -149,6 +159,9 @@ function animateScene() {
     boids
       .map((b) => {
         let colorObj = hexToRgb(b.color);
+        if (b.special) {
+          return [1, 1, 1, 1];
+        }
         return [colorObj.r, colorObj.g, colorObj.b, 1.0];
       })
       .reduce((acc, val) => acc.concat(val), [])
@@ -162,7 +175,6 @@ function animateScene() {
 
   gl.uniform2fv(uScalingFactor, currentScale);
   gl.uniform2fv(uRotationVector, currentRotation);
-  gl.uniform4fv(uGlobalColor, [0.4, 0.45, 0.4, 1.0]);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   const aVertexPosition = gl.getAttribLocation(
@@ -194,30 +206,255 @@ function animateScene() {
   gl.drawArrays(gl.POINTS, 0, vertexCount);
 
   window.requestAnimationFrame(function (currentTime) {
+    if (
+      document.getElementById("activeCheckbox") &&
+      !document.getElementById("activeCheckbox").checked
+    ) {
+      animateScene();
+      return;
+    }
     const velocity = ((currentTime - previousTime) / 1000.0) * boidsVelocity;
     previousTime = currentTime;
     boids.forEach((boid) => {
-      boid.xPos += velocity * Math.cos(boid.theta);
+      // boid.color = "#888888";
+      moveBoid(boid, velocity);
 
-      if (boid.xPos > glCanvas.width) {
-        boid.xPos -= 2 * glCanvas.width;
+      let separationData = [];
+      let alignmentData = [];
+      let cohesionData = [];
+
+      boids.forEach((other) => {
+        if (boidSeesBoid(boid, other)) {
+          separationData.push(getSeparationData(boid, other));
+          alignmentData.push(getAlignmentData(boid, other));
+          cohesionData.push(getCohesionData(boid, other));
+        }
+      });
+
+      let separationVector = calculateSeparationVector(boid, separationData);
+      let alignmentVector = calculateAlignmentVector(boid, alignmentData);
+      let cohesionVector = calculateCohesionVector(boid, cohesionData);
+
+      if (boid.special && separationData.length > 0) {
+        // console.log("SEPARATION", separationData, separationVector);
+        // console.log("ALIGNMENT", alignmentData, alignmentVector);
+        // console.log("COHESION", cohesionData, cohesionVector);
       }
 
-      if (boid.xPos < -glCanvas.width) {
-        boid.xPos += 2 * glCanvas.width;
+      let steerVector = calculateSteerVector(
+        boid,
+        separationVector,
+        alignmentVector,
+        cohesionVector
+      );
+
+      if (boid.special) {
+        // console.log("STEER", steerVector);
       }
 
-      boid.yPos += velocity * Math.sin(boid.theta);
-
-      if (boid.yPos > glCanvas.height) {
-        boid.yPos -= 2 * glCanvas.height;
-      }
-
-      if (boid.yPos < -glCanvas.height) {
-        boid.yPos += 2 * glCanvas.height;
-      }
+      boid = steerBoid(boid, steerVector);
     });
 
     animateScene();
   });
+}
+
+function moveBoid(boid, velocity) {
+  boid.xPos += velocity * Math.cos(boid.theta);
+
+  if (boid.xPos > glCanvas.width) {
+    boid.xPos -= 2 * glCanvas.width;
+  }
+
+  if (boid.xPos < -glCanvas.width) {
+    boid.xPos += 2 * glCanvas.width;
+  }
+
+  boid.yPos += velocity * Math.sin(boid.theta);
+
+  if (boid.yPos > glCanvas.height) {
+    boid.yPos -= 2 * glCanvas.height;
+  }
+
+  if (boid.yPos < -glCanvas.height) {
+    boid.yPos += 2 * glCanvas.height;
+  }
+}
+
+function boidSeesBoid(boid, other) {
+  viewport = (Math.PI / 3) * 2;
+
+  if (boid.xPos == other.xPos && boid.yPos == other.yPos) {
+    // other.color = "#888888";
+    return false;
+  }
+
+  const dist = Math.sqrt(
+    Math.pow(boid.xPos - other.xPos, 2) + Math.pow(boid.yPos - other.yPos, 2)
+  );
+
+  if (dist > 150) {
+    if (boid.special) other.color = "#888888";
+    return false;
+  }
+
+  const output = true;
+
+  if (boid.special && output) {
+    other.color = "#FAFA0A";
+  }
+
+  if (boid.special && !output) {
+    other.color = "#888888";
+  }
+
+  return output;
+}
+
+function getSeparationData(boid, other) {
+  let theta = Math.atan2(other.yPos - boid.yPos, other.xPos - boid.xPos);
+  let distance = Math.sqrt(
+    Math.pow(boid.xPos - other.xPos, 2) + Math.pow(boid.yPos - other.yPos, 2)
+  );
+  return {
+    theta: theta,
+    distance: distance,
+  };
+}
+
+function getAlignmentData(boid, other) {
+  return other.theta;
+}
+
+function getCohesionData(boid, other) {
+  return {
+    xPos: other.xPos,
+    yPos: other.yPos,
+  };
+}
+
+function calculateSeparationVector(boid, data) {
+  if (data.length == 0) {
+    return boid.theta;
+  }
+
+  const points = data.map(({ theta, distance }) => {
+    const thetaPlusPi = addPiToTheta(theta);
+    const x = Math.cos(thetaPlusPi) * distance;
+    const y = Math.sin(thetaPlusPi) * distance;
+    return {
+      xPos: x,
+      yPos: y,
+    };
+  });
+
+  const averageX = points.map((p) => p.xPos).reduce((a, b) => a + b);
+  const averageY = points.map((p) => p.yPos).reduce((a, b) => a + b);
+
+  const theta = Math.atan2(averageY, averageX);
+  return theta;
+}
+
+function calculateAlignmentVector(boid, data) {
+  if (data.length == 0) {
+    return boid.theta;
+  }
+
+  // take all thetas and turn into points
+  const points = data.map((t) => {
+    const xPos = Math.cos(t);
+    const yPos = Math.sin(t);
+    return {
+      xPos: xPos,
+      yPos: yPos,
+    };
+  });
+  // average components of all points
+  const averageX = points.map((p) => p.xPos).reduce((a, b) => a + b);
+  const averageY = points.map((p) => p.yPos).reduce((a, b) => a + b);
+
+  // get new theta from point
+  const theta = Math.atan2(averageY, averageX);
+
+  return theta;
+}
+
+function calculateCohesionVector(boid, data) {
+  if (data.length == 0) {
+    return boid.theta;
+  }
+
+  const averageX =
+    data.map((d) => d.xPos).reduce((a, b) => a + b) / data.length;
+  const averageY =
+    data.map((d) => d.yPos).reduce((a, b) => a + b) / data.length;
+
+  const theta = Math.atan2(averageY - boid.yPos, averageX - boid.xPos);
+  return theta;
+}
+
+function calculateSteerVector(boid, separationVec, alignmentVec, cohesionVec) {
+  const points = [
+    separationVec,
+    separationVec,
+    separationVec,
+    separationVec,
+    separationVec,
+    alignmentVec,
+    alignmentVec,
+    alignmentVec,
+    alignmentVec,
+    cohesionVec,
+    cohesionVec,
+    cohesionVec,
+    cohesionVec,
+  ].map((t) => {
+    const xPos = Math.cos(t);
+    const yPos = Math.sin(t);
+    return {
+      xPos: xPos,
+      yPos: yPos,
+    };
+  });
+  // average components of all points
+  const averageX = points.map((p) => p.xPos).reduce((a, b) => a + b);
+  const averageY = points.map((p) => p.yPos).reduce((a, b) => a + b);
+
+  // get new theta from point
+  const theta = Math.atan2(averageY, averageX);
+
+  return theta;
+}
+
+function steerBoid(boid, steerVec) {
+  const points = [steerVec, boid.theta, boid.theta, boid.theta].map((t) => {
+    const xPos = Math.cos(t);
+    const yPos = Math.sin(t);
+    return {
+      xPos: xPos,
+      yPos: yPos,
+    };
+  });
+  // average components of all points
+  const averageX = points.map((p) => p.xPos).reduce((a, b) => a + b);
+  const averageY = points.map((p) => p.yPos).reduce((a, b) => a + b);
+
+  // get new theta from point
+  const theta = Math.atan2(averageY, averageX);
+
+  boid.theta = theta;
+  return boid;
+}
+
+function addPiToTheta(theta) {
+  const piPlusTheta = Math.PI + theta;
+  if (piPlusTheta > 2 * Math.PI) {
+    return piPlusTheta - 2 * Math.PI;
+  }
+
+  if (piPlusTheta < 0) {
+    return piPlusTheta + 2 * Math.PI;
+  }
+
+  return piPlusTheta;
 }
