130544905f57f14cabe24631d0a773e953a030dc
James Little <littleguy23@gmail.com>
2020-08-12 18:51:33 -0700

    same old same old

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 $HOME/.cargo/bin/cargo install stork-search
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
-stork --build _site/stork-posts.toml
+$HOME/.cargo/bin/stork --build _site/stork-posts.toml
