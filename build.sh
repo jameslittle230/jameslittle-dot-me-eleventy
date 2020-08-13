b7747aa4dba7f8a3f65eb0412305aa79916b43a1
James Little <littleguy23@gmail.com>
2020-08-12 18:41:06 -0700

    noninteractive install

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install stork-search
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
 stork --build _site/stork-posts.toml
