275043a599a76d3f1fcd8f4a8d965b8a01c81e6c
James Little <littleguy23@gmail.com>
2020-08-23 10:18:52 -0700

    Pull from published build artifact

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-$HOME/.cargo/bin/cargo install stork-search
+wget https://github.com/jameslittle230/stork-gh-actions-tests/releases/download/refs%2Fheads%2Fgithub-action/stork-latest-x86_64-unknown-linux-gnu
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
-$HOME/.cargo/bin/stork --build _site/stork-posts.toml
+stork-latest-x86_64-unknown-linux-gnu --build _site/stork-posts.toml
