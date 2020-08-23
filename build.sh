a4765677ba0885219497e180d9ec32e474ad3192
James Little <littleguy23@gmail.com>
2020-08-23 10:20:23 -0700

    Fix path

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 wget https://github.com/jameslittle230/stork-gh-actions-tests/releases/download/refs%2Fheads%2Fgithub-action/stork-latest-x86_64-unknown-linux-gnu
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
-stork-latest-x86_64-unknown-linux-gnu --build _site/stork-posts.toml
+./stork-latest-x86_64-unknown-linux-gnu --build _site/stork-posts.toml
