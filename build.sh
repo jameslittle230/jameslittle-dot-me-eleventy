92fb9c03a30cdc85a27804fd1ce74b648404f20a
James Little <littleguy23@gmail.com>
2020-10-31 16:30:45 -0700

    search improvements

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-wget https://github.com/jameslittle230/stork-gh-actions-tests/releases/download/refs%2Fheads%2Fgithub-action/stork-latest-x86_64-unknown-linux-gnu
+wget https://github.com/jameslittle230/stork/releases/download/v0.7.4/stork-latest-x86_64-unknown-linux-gnu
 chmod +x stork-latest-x86_64-unknown-linux-gnu
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
 ./stork-latest-x86_64-unknown-linux-gnu --build _site/stork-posts.toml
