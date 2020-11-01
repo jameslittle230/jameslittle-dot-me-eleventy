f8e8f825d5d53aeeda216342d95add1f43c8c243
James Little <3058200+jameslittle230@users.noreply.github.com>
2020-10-31 17:06:12 -0700

    Fix dynamic guestbook entry loading (#13)

--- content/pages/guestbook.11tydata.js
+++ content/pages/guestbook.11tydata.js
@@ -5,6 +5,12 @@ async function fetchGuestbookEntries() {
     "https://vipqpoael1.execute-api.us-west-1.amazonaws.com/prod"
   );
 
+  if(process.env.ELEVENTY_ENV === "development") {
+    response.data.shift();
+  }
+
+  console.log(`Found ${response.data.length} guestbook entries`)
+
   return response.data;
 }
 
