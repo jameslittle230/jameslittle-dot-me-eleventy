fd635a2da54e8eb0b89d118e9e2cd95cb351609d
James Little <3058200+jameslittle230@users.noreply.github.com>
2020-12-05 12:46:47 -0500

    Fix margins in pull-wide content (#16)

--- content/scripts/matomo.js
+++ content/scripts/matomo.js
@@ -1,5 +1,3 @@
-<!-- Matomo -->
-<script type="text/javascript">
   var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -11,5 +9,3 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
-<!-- End Matomo Code -->
