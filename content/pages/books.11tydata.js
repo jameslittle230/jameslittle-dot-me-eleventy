066989f086e98537b02c2b54cbd4bda302d86496
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-01-13 19:59:18 -0500

    Get books from airtable (#20)
    * Fetch books from Airtable instead of hardcoded
    
    * gotta install airtable, oops
    
    * Process Airtable data after fetching it

---
+++ content/pages/books.11tydata.js
@@ -0,0 +1,80 @@
+const Airtable = require("airtable");
+const { zonedTimeToUtc, utcToZonedTime, format } = require("date-fns-tz");
+
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+
+Array.prototype.uniqued = function () {
+  var output = [];
+  for (const val of this) {
+    if (output.indexOf(val) === -1) {
+      output.push(val);
+    }
+  }
+  return output;
+};
+
+const booksBaseKey = "appktTl97d89xOZQa";
+var base = Airtable.base(booksBaseKey);
+
+const getAirtableEntries = new Promise((resolve, reject) => {
+  var books = [];
+
+  base("Reading List")
+    .select()
+    .eachPage(
+      (records, fetchNextPage) => {
+        // called for each page of records (100 records/page)
+
+        for (const record of records) {
+          const completed = utcToZonedTime(
+            new Date(record.get("Completion Date"))
+          );
+          const year = format(completed, "yyyy", { timeZone: "Etc/GMT" });
+          const datum = {
+            title: record.get("Title"),
+            author: record.get("Author"),
+            completed,
+            year,
+          };
+          books.push(datum);
+        }
+
+        fetchNextPage();
+      },
+      (err) => {
+        if (err) reject(err);
+        else resolve(books);
+      }
+    );
+});
+
+const processAirtableEntries = (data) => {
+  const years = data
+    .map((d) => d.year)
+    .uniqued()
+    .sort((a, b) => b - a); // reverse sort years
+
+  const yearListings = years.map((year) => ({
+    year,
+    books: data
+      .filter((d) => d.year === year)
+      .sort((a, b) => a.completed - b.completed),
+  }));
+
+  return {
+    count: data.length,
+    yearListings,
+  };
+};
+
+async function fetchBookData() {
+  const airtableEntries = await getAirtableEntries;
+  return processAirtableEntries(airtableEntries);
+}
+
+module.exports = async function () {
+  return await fetchBookData();
+};
