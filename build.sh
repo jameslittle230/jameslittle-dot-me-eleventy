b05c5a1f0a4e63eb0564e67e84a9a07d2329ecce
James Little <littleguy23@gmail.com>
2021-04-11 12:38:09 -0700

    i am a goober who cannot spell

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 wget https://github.com/jameslittle230/stork/releases/download/v1.1.0/stork-ubuntu-latest
 chmod +x stork-ubuntu-latest
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
-./stork-ubuntu-lateset --build _site/stork-posts.toml
+./stork-ubuntu-latest --build _site/stork-posts.toml
