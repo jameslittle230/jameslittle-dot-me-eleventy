b751f315f5dd6e56fa5a0745ca8ff6f497b7b69c
James Little <littleguy23@gmail.com>
2021-04-11 12:29:56 -0700

    Upgrade Stork; index all files as Markdown

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-wget https://github.com/jameslittle230/stork/releases/download/v0.7.4/stork-latest-x86_64-unknown-linux-gnu
+wget https://github.com/jameslittle230/stork/releases/download/v1.1.0/stork-ubuntu-latest
 chmod +x stork-latest-x86_64-unknown-linux-gnu
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
 ./stork-latest-x86_64-unknown-linux-gnu --build _site/stork-posts.toml
