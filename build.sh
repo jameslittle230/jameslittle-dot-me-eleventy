61f1ae396f38e4d7a39a5dcbfd4bb38f995822ce
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-04-26 23:00:22 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-wget https://github.com/jameslittle230/stork/releases/download/v1.1.0/stork-ubuntu-latest
+wget https://github.com/jameslittle230/stork/releases/download/v1.2.0/stork-ubuntu-16-04
 chmod +x stork-ubuntu-latest
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
 ./stork-ubuntu-latest --build _site/stork-posts.toml
