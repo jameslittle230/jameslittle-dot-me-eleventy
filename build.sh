f91c6e1dad4a9dde1ddb1c02d273b95c8fd58df5
James Little <jameslittle230@users.noreply.github.com>
2021-05-26 19:52:48 -0700

    Use new Stork config format & command line API

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 wget https://files.stork-search.net/releases/v1.2.1/stork-ubuntu-16-04
 chmod +x stork-ubuntu-16-04
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
-./stork-ubuntu-16-04 --build _site/stork-posts.toml
+./stork-ubuntu-16-04 build -i _site/stork-posts.toml -o _site/stork-posts.st
