ce6cfb5f4dd23c559c60fcaad6bdc11270777693
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-05-30 16:41:52 -0700

    Use latest version of Stork on CDN (#31)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-wget https://files.stork-search.net/releases/v1.2.1/stork-ubuntu-16-04
+wget https://files.stork-search.net/releases/latest/stork-ubuntu-16-04
 chmod +x stork-ubuntu-16-04
 ELEVENTY_ENV=production npx @11ty/eleventy --config=eleventy.js
 ./stork-ubuntu-16-04 build -i _site/stork-posts.toml -o _site/stork-posts.st
